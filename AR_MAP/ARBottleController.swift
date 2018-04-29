6c7552ffe6327218f901b53c4df4590fa8ee69e4
SeacenLiu <1016667972@qq.com>
2018-04-29 20:13:58 +0800

    点击漂流瓶做放大渐出动画。

--- AR_MAP/ARBottleController.swift
+++ AR_MAP/ARBottleController.swift
@@ -41,17 +41,44 @@ class ARBottleController: UIViewController {
         sceneView.session.pause()
     }
     
+    // MARK: - lazy
+    /// 创建漂流瓶结点
+    private lazy var bottleNode: SCNNode = {
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let color: UIColor = #colorLiteral(red: 0.3411764706, green: 0.9803921569, blue: 1, alpha: 1)
+        box.materials.first?.diffuse.contents = color
+        box.materials.first?.transparency = 0.5
+        let node = SCNNode(geometry: box)
+        return node
+    }()
+    
 }
 
 // MARK: - AR显示和交互
 extension ARBottleController: ARSCNViewDelegate {
     
+    @IBAction func addActionClick(_ sender: Any) {
+        SVProgressHUD.showInfo(withStatus: "寻找四周的漂流瓶")
+        SVProgressHUD.dismiss(withDelay: 1)
+        addBottle()
+    }
+    
     @objc func tapTest(recognizer: UITapGestureRecognizer) {
         print("tapTest")
         let point = recognizer.location(in: sceneView)
         let results = sceneView.hitTest(point, options: [SCNHitTestOption.boundingBoxOnly : true, SCNHitTestOption.firstFoundOnly: true])
-        results.forEach { (result) in
-            print(result)
+        if let _ = results.first {
+            // 瓶子做动画并离开界面
+            let scaleOut = SCNAction.scale(by: 2, duration: 0.2)
+            let fadeOut = SCNAction.fadeOut(duration: 0.2)
+            let group = SCNAction.group([scaleOut, fadeOut])
+            let sequence = SCNAction.sequence([group, SCNAction.removeFromParentNode()])
+            bottleNode.runAction(sequence) {
+                let scaleIn = SCNAction.scale(by: 0.5, duration: 0.1)
+                let fadeIn = SCNAction.fadeIn(duration: 0.1)
+                let groupIn = SCNAction.group([scaleIn, fadeIn])
+                self.bottleNode.runAction(groupIn)
+            }
         }
     }
     
@@ -61,33 +88,15 @@ extension ARBottleController: ARSCNViewDelegate {
     }
     
     private func addBottle() {
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        let color: UIColor = #colorLiteral(red: 0.3411764706, green: 0.9803921569, blue: 1, alpha: 1)
-        box.materials.first?.diffuse.contents = color
-        box.materials.first?.transparency = 0.5
-        let node = SCNNode(geometry: box)
-        sceneView.scene.rootNode.addChildNode(node)
-        let randNum = Double(arc4random())
+        let randNum = drand48() * 2.0
         let r = 1.0
         let x = r * cos(randNum * .pi)
-        let z = r * sin(randNum * .pi)
-        node.position = SCNVector3(x, 0, z)
+        let z = -r * sin(randNum * .pi)
+        print(randNum)
+        print(SCNVector3(x, 0, z))
+        bottleNode.position = SCNVector3(x, 0, z)
+        sceneView.scene.rootNode.addChildNode(bottleNode)
+        print(bottleNode)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //        let transform = matrix_float4x4()
-        //        let anchor = ARAnchor(transform: transform)
-        //        sceneView.session.add(anchor: anchor)
-    }
-    
-//    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-//        let color: UIColor = #colorLiteral(red: 0.3411764706, green: 0.9803921569, blue: 1, alpha: 1)
-//        box.materials.first?.diffuse.contents = color
-//        box.materials.first?.transparency = 0.5
-//        let node = SCNNode(geometry: box)
-//        print(node)
-//        print(anchor.transform)
-//        return node
-//    }
 }
