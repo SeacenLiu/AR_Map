cba3a13bc86e8c3d95d240e9ad0f5588c78fda99
SeacenLiu <1016667972@qq.com>
2018-05-01 15:47:07 +0800

    添加按钮demo.

---
+++ AR_MAP/添加视图/AddController.swift
@@ -0,0 +1,21 @@
+//
+//  AddController.swift
+//  AR_MAP
+//
+//  Created by SeacenLiu on 2018/5/1.
+//  Copyright © 2018年 成. All rights reserved.
+//
+
+import UIKit
+
+class AddController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addClick(_ sender: Any) {
+        print("点击了添加按钮")
+    }
+
+}
